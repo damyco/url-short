d4ff9ddc234d7ead53d2c774cc337374ee3c241a
damyco <25842807+damyco@users.noreply.github.com>
2022-06-24 16:46:43 +0100

    added footer & stats sections, CSS tweaks

--- components/ShortenLink.js
+++ components/ShortenLink.js
@@ -4,15 +4,22 @@ import styles from "../styles/ShortenLink.module.css";
 
 export default function ShortenLink() {
   return (
-    <section className={styles.wrapper}>
-      <form className={styles.container}>
-        <input
-          className={styles.linkInput}
-          type="url"
-          placeholder="Shorten a link here..."
-        />
+    <>
+      <section className={styles.wrapper}>
+        <form className={styles.container}>
+          <input
+            className={styles.linkInput}
+            type="url"
+            placeholder="Shorten a link here..."
+          />
+          <Button style="primary">Shorten it!</Button>
+        </form>
+      </section>
+      <div className={styles.linksContainer}>
+        <p>url</p>
+        <p>short url</p>
         <Button style="primary">Shorten it!</Button>
-      </form>
-    </section>
+      </div>
+    </>
   );
 }
